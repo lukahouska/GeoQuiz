a368bdf0c736cbf7768a3213a07aae7d5de1f545
Luka Houska <luka.houska@guest.arnes.si>
2022-10-03 00:26:45 +0200

    popravil štetje pravilnih odgovorov

--- tekstovni_vmesnik.py
+++ tekstovni_vmesnik.py
@@ -1,4 +1,4 @@
-from model import Igra
+from model import Igra1
 
 
 slovar_drzav = {"Slovenija":"LJUBLJANA", "Avstrija":"DUNAJ", "Nemčija":"BERLIN"}
@@ -7,19 +7,21 @@ slovar_drzav = {"Slovenija":"LJUBLJANA", "Avstrija":"DUNAJ", "Nemčija":"BERLIN"
 def pozeni_vmesnik():
     print("Dobrodošli v GeoQuiz")
     print("===========================")
-    igra = Igra()
-    seznam_drzav = Igra.pridobi_drzave(slovar_drzav)
+    igra = Igra1()
+    seznam_drzav = Igra1.pridobi_drzave(slovar_drzav)
+    stevilo_pravilnih = 0
     for i in range(2):
         print("drzava")
         print(seznam_drzav[i])
         odgovor = input("glavno mesto:")
-        pravilnost = Igra.preveri_odgovor(Igra, odgovor, slovar_drzav, seznam_drzav[i])
+        pravilnost = Igra1.preveri_odgovor(Igra1, odgovor, slovar_drzav, seznam_drzav[i])
         if pravilnost:
+            stevilo_pravilnih += 1
             print("Pravilen odgovor")
         else:
-            print("napacen odgovor")
+            print("Napačen odgovor")
 
-    print(f"Stevilo pravilnih je {igra.stevilo_pravilnih!r}")
+    print(f"Stevilo pravilnih je {stevilo_pravilnih!r}")
     
         
 
