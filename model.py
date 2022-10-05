2aa65b509d60e90dc5e51328ad98547f2208ba4a
Luka Houska <luka.houska@guest.arnes.si>
2022-10-05 03:07:49 +0200

    dodal funkcijo v nov class

--- model.py
+++ model.py
@@ -1,13 +1,52 @@
 # model za projektno
 import random
 
+
+class Kviz:
+    def __init__(self, skupne_tocke):
+        self.skupne_tocke = skupne_tocke
+
 class Igra1:
-    def __init__(self):
-        self.pravilni = 0
+    def __init__(self, pravilni):
+        self.pravilni = pravilni
 
-    def nova_igra():
-        pass
+    def pridobi_drzave(slovar):
+        seznam_drzav = list(slovar.keys())
+        stevilo_drzav = len(seznam_drzav)
+        nabor = []
+        while len(seznam_drzav) > 10:
+            a = random.randint(0, stevilo_drzav -1)
+            seznam_drzav.pop(a)
+            stevilo_drzav -= 1
+        for drzava in seznam_drzav:
+            nabor_moznosti = [drzava]
+            seznam_drzav1 = list(slovar.keys())
+            stevilo_drzav1 = len(seznam_drzav1)
+            a = seznam_drzav1.index(drzava)
+            seznam_drzav1.pop(a)
+            while len(seznam_drzav1) > 3:
+                b = random.randint(0, len(seznam_drzav1) -2)
+                seznam_drzav1.pop(b)
+                stevilo_drzav1 -= 1
+            nabor_moznosti.extend(seznam_drzav1)
+            random.shuffle(nabor_moznosti)
+            nabor.append(nabor_moznosti)
+        return nabor
+        
 
+
+
+
+
+
+
+
+
+
+
+class Igra2:
+    def __init__(self, pravilni):
+        self.pravilni = pravilni
 
     def pridobi_drzave(slovar):
         seznam_drzav = list(slovar.keys())
@@ -24,6 +63,8 @@ class Igra1:
         if odgovor == slovar_drzav[drzava]:
             return True
         return False
+
+
 
     
 
--- tekstovni_vmesnik.py
+++ tekstovni_vmesnik.py
@@ -1,4 +1,4 @@
-from model import Igra1
+from model import Igra1, Igra2, Kviz
 
 
 slovar_drzav = {"Slovenija":"LJUBLJANA", "Avstrija":"DUNAJ", "Nemčija":"BERLIN", "Francija":"PARIZ",
@@ -10,6 +10,8 @@ slovar_drzav = {"Slovenija":"LJUBLJANA", "Avstrija":"DUNAJ", "Nemčija":"BERLIN"
 
 
 def pozeni_vmesnik():
+    kviz = Kviz(0)
+
     print("Pozdravljeni!\nTa geografska igra je sestavljena iz dveh nivojev.\nZa napredovanje na naslednji nivo potrebujete vsaj 9/10 pravilnih odgovorov.\nZa vsak pravilni odgovor dobite eno točko.")
     print("=========================================")
 
@@ -18,57 +20,58 @@ def pozeni_vmesnik():
     print("\nMed štirimi možnimi odgovori državi določite pripadajoče glavno mesto.")
 
 
-    igra = Igra1()
+    igra1 = Igra1(0)
     seznam_drzav = Igra1.pridobi_drzave(slovar_drzav)
-    stevilo_pravilnih = 0
-    skupne_tocke = 0
+    #stevilo_pravilnih = 0
+    #skupne_tocke = 0
     for i in range(10):
         print("drzava:" + seznam_drzav[i])
         odgovor = input("glavno mesto:")
-        pravilnost = Igra1.preveri_odgovor(Igra1, odgovor, slovar_drzav, seznam_drzav[i])
+        pravilnost = Igra1.preveri_odgovor(igra1, odgovor, slovar_drzav, seznam_drzav[i])
         if pravilnost:
-            stevilo_pravilnih += 1
-            skupne_tocke += 1
+            #stevilo_pravilnih += 1
+            igra1.pravilni += 1
+            kviz.skupne_tocke += 1
             print("Pravilen odgovor")
         else:
             print("Napačen odgovor")
-    if stevilo_pravilnih >= 9:
-        print(f"Pravilno ste odgovorili na {stevilo_pravilnih!r}/10 vprašanj.")
+    if igra1.pravilni >= 9:
+        print(f"Pravilno ste odgovorili na {igra1.pravilni!r}/10 vprašanj.")
     else:
-        print(f"Pravilno ste odgovorili na {stevilo_pravilnih!r}/10 vprašanj. Na žalost to ni dovolj za naslednji nivo.")
+        print(f"Pravilno ste odgovorili na {igra1.pravilni!r}/10 vprašanj. Na žalost to ni dovolj za naslednji nivo.")
         
 
 #nova stran
-    if stevilo_pravilnih >= 9:
+    if igra1.pravilni >= 9:
         print("LEVEL 2")
         print("\nZa vsako državo pripišite pripadajoče glavno mesto.")
 
-        igra = Igra1()
-        seznam_drzav = Igra1.pridobi_drzave(slovar_drzav)
-        stevilo_pravilnih = 0
+        igra2 = Igra2(0)
+        seznam_drzav = Igra2.pridobi_drzave(slovar_drzav)
+        #stevilo_pravilnih = 0
         for i in range(10):
             print("drzava:" + seznam_drzav[i])
             odgovor = input("glavno mesto:")
-            pravilnost = Igra1.preveri_odgovor(Igra1, odgovor, slovar_drzav, seznam_drzav[i])
+            pravilnost = Igra2.preveri_odgovor(igra2, odgovor, slovar_drzav, seznam_drzav[i])
             if pravilnost:
-                stevilo_pravilnih += 1
-                skupne_tocke += 1
+                igra2.pravilni += 1
+                kviz.skupne_tocke += 1
                 print("Pravilen odgovor")
             else:
                 print("Napačen odgovor")
             
-        print(f"\nPravilno ste odgovorili na {stevilo_pravilnih!r}/10 vprašanj.")
-    elif stevilo_pravilnih < 9:
+        print(f"\nPravilno ste odgovorili na {igra2.pravilni!r}/10 vprašanj.")
+    elif igra2.pravilni < 9:
         pass
 
 #level 3?
 #nova stran
-    if skupne_tocke < 10:   
-        print(f"Očitno geografija ni vaše področje. Skupno število točk: {skupne_tocke!r}.\nNa prvi strani si lahko pogledate na katero mesto vas to uvršča.")
-    elif 10 <= skupne_tocke <= 15:
-        print(f"Soliden rezultat vendar zmorete bolje. Skupaj ste zbrali {skupne_tocke!r} točk.\nNa prvi strani si lahko pogledate na katero mesto vas to uvršča.")
-    elif skupne_tocke > 15:
-        print(f"Odlično! Skupaj ste zbrali {skupne_tocke!r} točk.\nNa prvi strani si lahko pogledate na katero mesto vas to uvršča.")
+    if kviz.skupne_tocke < 10:   
+        print(f"Očitno geografija ni vaše področje. Skupno število točk: {kviz.skupne_tocke!r}.\nNa prvi strani si lahko pogledate na katero mesto vas to uvršča.")
+    elif 10 <= kviz.skupne_tocke <= 15:
+        print(f"Soliden rezultat vendar zmorete bolje. Skupaj ste zbrali {kviz.skupne_tocke!r} točk.\nNa prvi strani si lahko pogledate na katero mesto vas to uvršča.")
+    elif kviz.skupne_tocke > 15:
+        print(f"Odlično! Skupaj ste zbrali {kviz.skupne_tocke!r} točk.\nNa prvi strani si lahko pogledate na katero mesto vas to uvršča.")
         
 
 
